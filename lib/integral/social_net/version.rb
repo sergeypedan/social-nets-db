0e98e0f07766c4e4b873aea4ffca04e33837fba6
Sergey Pedan <sergey.pedan@gmail.com>
2019-05-11 03:15:17 +0700

    Adds tests

--- lib/integral/social_net/version.rb
+++ lib/integral/social_net/version.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module Integral
   class SocialNet
-    VERSION = "0.1.0"
+    VERSION = "0.1.0".freeze
   end
 end
---
+++ spec/initializer_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Integral::SocialNet, ".initialize" do
+
+  context "with valid uid" do
+    it do
+      expect(described_class.new("facebook")).to be_a Integral::SocialNet
+    end
+  end
+
+  context "with unsupported uid" do
+    it do
+      expect { described_class.new("blabla") }.to raise_exception ArgumentError
+    end
+  end
+
+  context "with empty uid" do
+    it do
+      expect { described_class.new("") }.to raise_exception ArgumentError
+    end
+  end
+
+  context "with no arguments" do
+    it do
+      expect { described_class.new }.to raise_exception ArgumentError
+    end
+  end
+
+end
